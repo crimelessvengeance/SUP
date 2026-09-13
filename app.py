1539e05f1c782b0e68f97eb7d2786a259f59ab57
crimelessvengeance <sayan07nandi@gmail.com>
2026-09-13 23:08:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -280,7 +280,7 @@ if prompt:
             )
 
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.6-flash",
             contents=contents
         )
 
